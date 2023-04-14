545b1c8d2ee7059458cb4dfb435bee3a901f511b
Renzo Declerck <renzo.declerck@student.ap.be>
2023-04-14 12:24:50 +0200

    Update showInfo(el)

--- js/history.js
+++ js/history.js
@@ -2,7 +2,7 @@
 function showInfo(element) {
     document.getElementById("company-info").style.display = "block";
     document.addEventListener("click", (event) => {
-        if (!element.contains(event.target)) {
+        if (document.getElementById("company-info").contains(event.target)) {
             document.getElementById("company-info").style.display = "none";
         };
     })
