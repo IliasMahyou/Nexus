5cb18cedd39963c6c620977b63fe0c1bfda9f079
Renzo Declerck <renzo.declerck@student.ap.be>
2023-04-14 13:30:47 +0200

    Toevoeging hideInfo-btn en hideInfo()

--- js/history.js
+++ js/history.js
@@ -1,9 +1,8 @@
 //Bedrijfsinfovenster tevoorschijn laten komen bij het kiezen van een bedrijf uit de historiek.
 function showInfo(element) {
     document.getElementById("company-info").style.display = "block";
-    document.addEventListener("click", (event) => {
-        if (document.getElementById("company-info").contains(event.target)) {
-            document.getElementById("company-info").style.display = "none";
-        };
-    })
+}
+//Bedrijfsinfovenster laten verdwijnen bij het klikken op de knop.
+function hideInfo() {
+    document.getElementById("company-info").style.display = "none";
 }
