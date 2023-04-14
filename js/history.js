cc0d0f002179aa7ad6e0680b9519bbad5f70c7d5
Renzo Declerck <renzo.declerck@student.ap.be>
2023-04-14 12:02:01 +0200

    Update showInfo(el)

--- js/history.js
+++ js/history.js
@@ -1,4 +1,9 @@
 //Bedrijfsinfovenster tevoorschijn laten komen bij het kiezen van een bedrijf uit de historiek.
 function showInfo(element) {
     document.getElementById("company-info").style.display = "block";
+    document.addEventListener("click", (event) => {
+        if (!element.contains(event.target)) {
+            document.getElementById("company-info").style.display = "none";
+        };
+    })
 }
