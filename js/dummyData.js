b872d002b7be835998120c9d4f6486015e1c449e
Renzo Declerck <renzo.declerck@student.ap.be>
2023-04-16 22:57:12 +0200

    Dummy-werking datafetching

--- js/dummyData.js
+++ js/dummyData.js
@@ -1,7 +1,5 @@
 const frmComparison = document.forms.namedItem("frmComparison");
-const company1Number = parseInt(frmComparison["company1"].value);
-const company2Number = parseInt(frmComparison["company2"].value);
-let equities, debts, profits; 
+let equities = [], debts = [], profits = []; 
 
 const company1Data = {
     name: "CauSup",
@@ -16,51 +14,35 @@ const company2Data = {
     address: "koekelstraat 3",
     date: '12/12/2022',
     equity: 10.000,
-    debts: 8.000,
+    debts: 9.000,
     profit: 2.000
 }
 
-function fetchCompanyData() {
-    if (company1Number === 485362) {
-        document.getElementById("company1Name").textContent = company1Data.name;
-        document.getElementById("company1Address").textContent = company1Data.address;
-        document.getElementById("company1Date").textContent = company1Data.date;
-        document.getElementById("company1Equity").textContent = company1Data.equity;
-        document.getElementById("company1Debts").textContent = company1Data.debts;
-        document.getElementById("company1Profit").textContent = company1Data.profit;
-        showError(1, false);
-        equities[0] = company1Data.equity;
-        debts[0] = company1Data.debts;
-        profits[0] = company1Data.debts;
-    } else {
-        showError(1, true);
-    }
-    if (company2Number === 569082) {
-        document.getElementById("company2Name").textContent = company2Data.name;
-        document.getElementById("company2Address").textContent = company2Data.address;
-        document.getElementById("company2Date").textContent = company2Data.date;
-        document.getElementById("company2Equity").textContent = company2Data.equity;
-        document.getElementById("company2Debts").textContent = company2Data.debts;
-        document.getElementById("company2Profit").textContent = company2Data.profit;
-        showError(2, false);
-        equities[1] = company2Data.equity;
-        debts[1] = company2Data.debts;
-        profits[1] = company2Data.debts;
-    } else {
-        showError(2, true);
-    }
-}
-  
-function showError(company, state) {
-    if (state) {
-        document.getElementById(`company${company}-input`).insertAdjacentHTML("afterend", '<span class="errMessage"> Onbekend ondernemingsnummer. Probeer opnieuw');
-    }
-    else {
-        document.getElementsByClassName("errMessage");
-        for (const errMessage in errMessage) {
-            document.removeChild(errMessage);
+function fetchCompanyData(company) {
+        if (company == 1) {
+            document.getElementById("company1Name").textContent = company1Data.name;
+            document.getElementById("company1Address").textContent = company1Data.address;
+            document.getElementById("company1Date").textContent = company1Data.date;
+            document.getElementById("company1Equity").textContent = '€' + company1Data.equity + '.000,00';
+            document.getElementById("company1Debts").textContent = '€' + company1Data.debts + '.000,00';
+            document.getElementById("company1Profit").textContent = '€' + company1Data.profit + '.000,00';
+            equities[0] = company1Data.equity;
+            debts[0] = company1Data.debts;
+            profits[0] = company1Data.debts;
         }
-    }
+        if (company == 2) {
+            document.getElementById("company2Name").textContent = company2Data.name;
+            document.getElementById("company2Address").textContent = company2Data.address;
+            document.getElementById("company2Date").textContent = company2Data.date;
+            document.getElementById("company2Equity").textContent = '€' + company2Data.equity + '.000,00';
+            document.getElementById("company2Debts").textContent = '€' + company2Data.debts + '.000,00';
+            document.getElementById("company2Profit").textContent = '€' + company2Data.profit + '.000,00';
+            equities[1] = company2Data.equity;
+            debts[1] = company2Data.debts;
+            profits[1] = company2Data.profits;
+        }
+        markDifferences(equities, debts, profits);
+    
 }
 
 function markDifferences(equities, debts, profits) {
@@ -85,9 +67,39 @@ function markDifferences(equities, debts, profits) {
         document.getElementById("company1Profit").style.color = "red";
         document.getElementById("company2Profit").style.color = "green";
     }
+
+    //Nog kleur maken voor even aantal
+}
+
+document.getElementById("btnCompare").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+function deleteErr() {
+    const errMessages = document.getElementsByClassName("errMessage");
+    for (let i= 0; i < errMessages.length; i++) {
+       errMessages[i].textContent = "";
+    }
 }
 
 function compare() {
-    fetchCompanyData();
-    markDifferences();
+    const company1Number = frmComparison["company1"].value;
+    const company2Number = frmComparison["company2"].value;
+    let counter = 0;
+    if (company1Number == 485362) {
+        fetchCompanyData(1);
+        counter++;
+    } else {
+        frmComparison.insertAdjacentHTML("afterend", '<span class="errMessage"> Eerste ondernemingsnummer is niet gevonden.</span>');
+    }
+    if (company2Number == 569082) {
+        fetchCompanyData(2);
+        counter++;
+    } else {
+        frmComparison.insertAdjacentHTML("afterend", '<span class="errMessage"> Tweede ondernemingsnummer is niet gevonden.</span>');
+    }
+    if (counter == 2) {
+        deleteErr();
+    }
+    //Misschien dan nog later, een foutmelding laten verschijnen wanneer de gebruiker wil vergelijken maar nog geen ondernemingsnummers heeft ingegeven
 }
