37ff682e84e21484cf29e83ec0d6e599e8282800
Renzo Declerck <renzo.declerck@student.ap.be>
2023-04-16 10:54:46 +0200

    Dummy-data (d1)

---
+++ js/dummyData.js
@@ -0,0 +1,10 @@
+//Voorlopige code, dit bestand is te verwijderen na de demonstratie van de front-end!
+
+//Te hergebruiken constanten
+const frmComparison = document.forms.namedItem('frmComparison');
+const company1Number = parseInt(frmComparison["company1"].value);
+const company2Number = parseInt(frmComparison["company2"].value);
+
+if (company1Number === 485362) {
+    
+}
