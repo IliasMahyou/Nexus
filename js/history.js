6531adeebb5071b4257b494bbae728bcef8e7ca1
Warre Bogaerts <s142255@ap.be>
2023-04-18 23:39:07 +0200

    translateANDdarkLight branche klaar

--- js/history.js
+++ js/history.js
@@ -1,8 +1,29 @@
+let searchedCompanies = [];
 //Bedrijfsinfovenster tevoorschijn laten komen bij het kiezen van een bedrijf uit de historiek.
-function showInfo(element) {
+function showInfo(companyName) {
+    let company;
+    for (let i = 0; i < searchedCompanies.length; i++) {
+        if (searchedCompanies[i].name === companyName) {
+            company = searchedCompanies[i];
+        }
+    }
     document.getElementById("company-info").style.display = "block";
+    document.getElementById("companyName").textContent = company.name;
+    document.getElementById("companyAddress").textContent = company.address;
+    document.getElementById("companyDate").textContent = company.date;
+    document.getElementById("companyEquity").textContent = company.equity;
+    document.getElementById("companyDebts").textContent = company.debts;
+    document.getElementById("companyProfit").textContent = company.profit;
 }
 //Bedrijfsinfovenster laten verdwijnen bij het klikken op de knop.
 function hideInfo() {
     document.getElementById("company-info").style.display = "none";
+}
+//Bedrijf na het vergelijken toevoegen in de historiek.
+function addToHistory(company) {
+    const history = document.querySelector("#history ul");
+    if (!(searchedCompanies.includes(company))) {
+        searchedCompanies.push(company);
+        history.insertAdjacentHTML("afterbegin", `<li onclick="showInfo(this.textContent)">${company.name}</li>`);
+    }
 }
--- js/translateHome.js
+++ js/translateHome.js
@@ -54,7 +54,7 @@ let data = {
         para: "Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.",
         titleHome: "Outil de comparaison",
         compareButton: "Comparer",
-        th: ["Nom:", "Adres:", "Date de dépôt:", "Capital:", "Dettes:", "Résultat d'exploitation:", "Nom:", "Adres:", "Date de dépôt:", "Capital:", "Dettes:", "Résultat d'exploitation:"],
+        th: ["Nom:", "Adresse:", "Date de dépôt:", "Capital:", "Dettes:", "Résultat d'exploitation:", "Nom:", "Adresse:", "Date de dépôt:", "Capital:", "Dettes:", "Résultat d'exploitation:"],
         translateHistory: "Recent searches",
         li: ["Entreprise A", "Entreprise B","Entreprise C", "Entreprise D","Entreprise E", "Entreprise F", "Entreprise G", "Entreprise H","Entreprise I", "Entreprise J"]
 
