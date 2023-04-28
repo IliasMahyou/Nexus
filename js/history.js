5e5886dcf11c74877621af943917be6ddb29f162
Renzo Declerck <renzo.declerck@student.ap.be>
2023-04-28 10:53:22 +0200

    addToHistory(company) verwijderd

--- js/history.js
+++ js/history.js
@@ -1,5 +1,3 @@
-let searchedCompanies = [];
-
 function fillIn(id, content) {
     document.getElementById(id).textContent = content;
 }
@@ -13,15 +11,5 @@ function showCompanyInfo(company) {
     fillIn("companyEquity", company.equity);
     fillIn("companyDebts", company.debts);
     fillIn("companyProfit", company.profit);
-}
-
-///////////////DEZE FUNCTIE NAAR ANDER HOME/COMPARE-SCRIPT brengen
-
-//Bedrijf na het vergelijken toevoegen in de historiek.
-function addToHistory(company) {
-    const history = document.querySelector("#history ul");
-    if (!(searchedCompanies.includes(company))) {
-        searchedCompanies.push(company);
-        history.insertAdjacentHTML("afterbegin", `<li onclick="showInfo(this.textContent)">${company.name}</li>`);
-    }
+    document.getElementById("section-companyInfo").style.display = "block";
 }
