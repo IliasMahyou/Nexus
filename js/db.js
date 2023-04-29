1a3bff3f998a6f573549494b73d5bc5e65e40a4d
Renzo Declerck <renzo.declerck@student.ap.be>
2023-04-29 18:26:45 +0200

    Toevoeging fetchCompanyData(referencenumber)

--- js/db.js
+++ js/db.js
@@ -3,3 +3,9 @@ const {MongoClient} = require('mongodb');//MongoDB-functionaliteiten
 /*Overige constantendeclaraties*/
 const connection = "";//Connectiestring met de db
 const client = new MongoClient(connection, {useUnifiedTopology: true});//Connectieclient
+/*Asynchrone arrowfuncties*/
+const fetchCompanyData = async (referencenumber) => {
+    await client.connect();
+    const company = await client.db('NBB').collection('companies').findOne({referencenumber: referencenumber});
+    return company;
+}
