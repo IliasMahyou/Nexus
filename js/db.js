9f6209108361657a9fa10641afaa45f81672f411
Renzo Declerck <renzo.declerck@student.ap.be>
2023-05-01 18:30:27 +0200

    Toevoeging comdpanyExist(referencenumber)

--- js/db.js
+++ js/db.js
@@ -3,6 +3,13 @@ const {MongoClient} = require('mongodb');
 const connection = "mongodb+srv://nexus_admin:Nexus_123@nexus.09eb4ta.mongodb.net/Nexus?retryWrites=true&w=majority";
 const client = new MongoClient(connection, {useUnifiedTopology: true});
 /*Asynchrone functies*/
+async function companyExist(referencenumber) {
+    if (fetchCompany(referencenumber) == null) {
+        return true;
+    } else {
+        return false;
+    }
+}
 async function fetchCompany(referencenumber) {
     await client.connect();
     const company = await client.db('NBB').collection('Companies').findOne({referencenumber: referencenumber});
