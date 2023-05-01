6ee105b7e96e3a872842bac43d58eaf6d446d95d
Renzo Declerck <renzo.declerck@student.ap.be>
2023-05-01 18:20:28 +0200

    Toevoeging addCompany(company)

--- js/db.js
+++ js/db.js
@@ -1,11 +1,18 @@
 /*Imporatie*/
-const {MongoClient} = require('mongodb');//MongoDB-functionaliteiten
-/*Overige constantendeclaraties*/
-const connection = "mongodb+srv://nexus_admin:Nexus_123@nexus.09eb4ta.mongodb.net/Nexus?retryWrites=true&w=majority";//Connectiestring met de db
-const client = new MongoClient(connection, {useUnifiedTopology: true});//Connectieclient
+const {MongoClient} = require('mongodb');
+const connection = "mongodb+srv://nexus_admin:Nexus_123@nexus.09eb4ta.mongodb.net/Nexus?retryWrites=true&w=majority";
+const client = new MongoClient(connection, {useUnifiedTopology: true});
 /*Asynchrone functies*/
-async function fetchCompanyData(referencenumber) {
+async function fetchCompany(referencenumber) {
     await client.connect();
     const company = await client.db('NBB').collection('Companies').findOne({referencenumber: referencenumber});
+    await client.close();
     return company;
+}
+async function addCompany(company){
+    if (!compannyExist) {
+        await client.connect();
+        await client.db('NBB').collection('Companies').insertOne(company);
+        await client.close();
+    }
 }
