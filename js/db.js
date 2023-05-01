66e433db05544e34c5d006b65242d92eb2cf1d3a
Renzo Declerck <renzo.declerck@student.ap.be>
2023-05-01 18:10:54 +0200

    Wijziging fetchCompanyData(referencenumber)

--- js/db.js
+++ js/db.js
@@ -1,11 +1,11 @@
 /*Imporatie*/
 const {MongoClient} = require('mongodb');//MongoDB-functionaliteiten
 /*Overige constantendeclaraties*/
-const connection = "";//Connectiestring met de db
+const connection = "mongodb+srv://nexus_admin:Nexus_123@nexus.09eb4ta.mongodb.net/Nexus?retryWrites=true&w=majority";//Connectiestring met de db
 const client = new MongoClient(connection, {useUnifiedTopology: true});//Connectieclient
-/*Asynchrone arrowfuncties*/
-const fetchCompanyData = async (referencenumber) => {
+/*Asynchrone functies*/
+async function fetchCompanyData(referencenumber) {
     await client.connect();
-    const company = await client.db('NBB').collection('companies').findOne({referencenumber: referencenumber});
+    const company = await client.db('NBB').collection('Companies').findOne({referencenumber: referencenumber});
     return company;
 }
