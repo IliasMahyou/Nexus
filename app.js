2a3267077c9bb303600b5d4d782d2c33f421ac78
Renzo Declerck <renzo.declerck@student.ap.be>
2023-05-03 10:25:00 +0200

    Wijzigingen app.js en toevoeging type-attribuut in package.json

---
+++ app.js
@@ -0,0 +1,34 @@
+/*Importatie*/
+import express from "express";
+import ejs from "ejs";
+
+
+/*Constantedeclaratie*/
+const app = express();
+
+
+/*Synchrone functies*/
+app.set("view engine", "ejs");
+app.set("port", 3000);
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+app.get("/login", (req, res) => {
+    res.render('login');
+});
+app.get("/home", (req, res) => {
+    res.render('home');
+  });
+app.get("/history", (req, res) => {
+    res.render('history');
+});app.get("/about", (req, res) => {
+    res.render('about');
+});
+app.get("/contact", (req, res) => {
+    res.render('contact');
+});
+
+app.listen(app.get("port"), () =>
+  console.log("[server] http://localhost:" + app.get("port"))
+);
