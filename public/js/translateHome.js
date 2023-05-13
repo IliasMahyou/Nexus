a7d2c16bb4ee86fd274bb9a5838a24452cbd9eb8
Milan Mareels <milan.mareels@gmail.com>
2023-05-13 10:39:15 +0200

    landing Page aanpasingen + translate teksten in orde gebracht

--- public/js/translateHome.js
+++ public/js/translateHome.js
@@ -28,7 +28,7 @@ link.forEach((el) => {
 let data = {
   dutch: {
     talen: "NL",
-    para: "Lorem Ipsum is een eenvoudige dummytekst van de druk- en zetterijindustrie. Lorem Ipsum is in de zeventiende eeuw als 'een normale tekst' gebruikt. Hierdoor is het niet alleen vijf eeuwen, maar ook de sprong naar elektronische typesetting, blijvend essentieel. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages, en meer recentelijk door desktop publishing software zoals Aldus PageMaker met versies van Lorem Ipsum.",
+    para: "Welkom bij Nexus, waar we ons richten op het leveren van innovatieve en nuttige tools om het leven van onze klanten gemakkelijker te maken. Onderaan de pagina vindt u een unieke vergelijkingstool waarmee de jaarrekeningen van twee bedrijven met elkaar vergeleken kunnen worden. Deze tool is ontwikkeld om u te helpen weloverwogen beslissingen te nemen bij het beoordelen van de financiële gezondheid van bedrijven. Onze vergelijkingstool is eenvoudig in gebruik en biedt u snel en nauwkeurig inzicht in de financiële prestaties van de bedrijven die u vergelijkt.",
     titleHome: "Vergelijkingstool",
     compareButton: "Vergelijk",
     th: [
@@ -61,7 +61,7 @@ let data = {
   },
   english: {
     talen: "EN",
-    para: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    para: "Welcome to Nexus, where we focus on providing innovative and useful tools to make our customers' lives easier. At the bottom of the page you will find a unique comparison tool with which the annual accounts of two companies can be compared. This tool is designed to help you make informed decisions when assessing the financial health of companies. Our comparison tool is easy to use and gives you a quick and accurate insight into the financial performance of the companies you compare.",
     titleHome: "comparison tool",
     compareButton: "Compare",
     th: [
@@ -94,7 +94,7 @@ let data = {
   },
   french: {
     talen: "FR",
-    para: "Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.",
+    para: "Bienvenue chez Nexus, où nous nous concentrons sur la fourniture d'outils innovants et utiles pour faciliter la vie de nos clients. En bas de page, vous trouverez un outil de comparaison unique avec lequel les comptes annuels de deux entreprises peuvent être comparés. Cet outil est conçu pour vous aider à prendre des décisions éclairées lors de l'évaluation de la santé financière des entreprises. Notre outil de comparaison est facile à utiliser et vous donne un aperçu rapide et précis de la performance financière des entreprises que vous comparez.",
     titleHome: "Outil de comparaison",
     compareButton: "Comparer",
     th: [
@@ -125,4 +125,4 @@ let data = {
       "Entreprise J",
     ],
   },
-}
+};
