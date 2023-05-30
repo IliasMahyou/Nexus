a7497771ee1e5bf6f511d231b43c14072e79912f
Renzo Declerck <renzo.declerck@student.ap.be>
2023-05-30 14:53:11 +0200

    Cleanup: db.ts (DY)

--- db.ts
+++ db.ts
@@ -17,21 +17,6 @@ async function fetchCompany(referencenumber: string): Promise<WithId<Document> |
     .collection("Companies")
     .findOne({ referencenumber: referencenumber });
 }
-//Voegt een bedrijf toe in de tabel 'Companies'
-//@param  company Het toe te voegen bedrijf
-async function addCompany(company: Company): Promise<void>{ 
-  await client
-    .db("NBB")
-    .collection("Companies")
-    .updateOne(company, { $set: company }, { upsert: true });
-}
-//Gaat na of een gebruiker bestaat in de databank
-//@param  userData  De op te vragen gebruikersdata
-//@return True indien de gebruikersdata terug kan gevonden worden, anders false
-async function userExist(userData: User): Promise<Boolean>{
-  const user = await client.db("NBB").collection("Users").findOne({ name: userData.username, password: userData.password });//De eventueel gevonden gebruiker
-  return user == null ? true : false;  //Als de gebruiker kan worden gevonden, wat er true teruggegeven, anders false
-}
 //Haalt de zoekgeschiedenis op van de meegegeven gebruiker
 //@param  username  De gebruikersnaam van de gebruiker
 //@return Een reeks met alle zoekopdrachten van de gebruiker
@@ -41,6 +26,14 @@ async function fetchHistory(username: string): Promise<WithId<Document>[]>{
     .collection("History")
     .find({ user: username })
     .toArray();
+}
+//Voegt een bedrijf toe in de tabel 'Companies'
+//@param  company Het toe te voegen bedrijf
+async function addCompany(company: Company): Promise<void>{ 
+  await client
+    .db("NBB")
+    .collection("Companies")
+    .updateOne(company, { $set: company }, { upsert: true });
 }
 //Voegt een zoekopdracht toe in de tabel 'History'
 //@param  search  De toe te voegen zoekopdracht
@@ -52,5 +45,5 @@ async function addToHistory(search: History): Promise<void>{
 }
 
 /*Exportatie*/
-export { userExist, fetchHistory, fetchCompany, connect, addToHistory, addCompany }//De te gebruiken functies
+export { connect, fetchCompany, fetchHistory, addCompany, addToHistory }//De te gebruiken functies
 export { client }//De te gebruiken constanten
