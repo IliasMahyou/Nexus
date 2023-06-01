8471bddf443ab95502a2b11593447119eccac236
Renzo Declerck <renzo.declerck@student.ap.be>
2023-06-01 21:42:39 +0200

    Cleanup: darkLight.js

--- public/js/darkLight.js
+++ public/js/darkLight.js
@@ -1,31 +1,37 @@
-const body = document.querySelector("body");
-const toggle = document.querySelector("#toggle");
-const sunIcon = document.querySelector(".toggle .bxs-sun");
-const moonIcon = document.querySelector(".toggle .bx-moon");
+/*Constanten*/
+const body = document.getElementsByTagName('body')[0];//De pagina
+const toggle = document.getElementById('toggle');//De kleurmodusknop
+const sunIcon = document.getElementsByClassName(".bxs-sun")[0];//Het zon-icoontje
+const moonIcon = document.getElementsByClassName(".bx-moon")[0];//Het maan-icoontje
+const savedMode = localStorage.getItem("mode");//De huidige kleurmodus
 
-const savedMode = localStorage.getItem("mode");
+/*Functies*/
+//Activeert donkere modus en bewaart deze keuze
+function enableDarkMode() {
+  body.classList.add("body--dark");
+  sunIcon.className = "bx bx-sun";
+  moonIcon.className = "bx bxs-moon";
+  localStorage.setItem("mode", "body--dark");
+}
+//Deactiveert donkere modus en bewaart deze keuze
+function disableDarkMode() {
+  body.classList.remove("body--dark");
+  sunIcon.className = "bx bxs-sun";
+  moonIcon.className = "bx bx-moon";
+  localStorage.setItem("mode", "light");
+}
+
+
+//Als de huidige kleurmodus 'donker' is, dan wordt de donkere modus ingeschakeld
 if (savedMode === "dark") {
   enableDarkMode();
 }
-
+//Kleurmodus wijzigen bij het togglen van de kleurmodusknop
 toggle.addEventListener("change", () => {
-  if (body.classList.contains("dark")) {
+  //Als de huidige webpagina in donkere modus stond, dan wordt de webpagina in lichte modus gezet, anders omgekeerd 
+  if (body.classList.contains("body--dark")) {
     disableDarkMode();
   } else {
     enableDarkMode();
   }
 });
-
-function enableDarkMode() {
-  body.classList.add("dark");
-  sunIcon.className = "bx bx-sun";
-  moonIcon.className = "bx bxs-moon";
-  localStorage.setItem("mode", "dark");
-}
-
-function disableDarkMode() {
-  body.classList.remove("dark");
-  sunIcon.className = "bx bxs-sun";
-  moonIcon.className = "bx bx-moon";
-  localStorage.setItem("mode", "light");
-}
